e2cc6932911048cfee401beb7b4acfff0eb76d01
mikeevmm <miguelmurca@gmail.com>
2021-01-07 23:27:53 +0000

    feat: materials for pypi upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", 'r', encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+setuptools.setup(
+        name="sane-miguelmurca",
+        version="2.0",
+        author="Miguel Murça",
+        author_email="miguel.murca+pypi@gmail.com",
+        description="Make, but Sane",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/mikeevmm/sane",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: GNU/Linux",
+        ],
+        python_requires='>=3.6',
+)
