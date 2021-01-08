2142dfc998adba7dc86fbe8927845dd3112c71fa
mikeevmm <miguelmurca@gmail.com>
2021-01-08 00:19:42 +0000

    fix!: that was awful; proper pip state

--- setup.py
+++ setup.py
@@ -4,14 +4,17 @@ with open("README.md", 'r', encoding="utf-8") as readme_file:
     long_description = readme_file.read()
 
 setuptools.setup(
-        name="sane-miguelmurca",
+        name="sane-build",
         version="2.0",
         author="Miguel Murça",
         author_email="miguel.murca+pypi@gmail.com",
         description="Make, but Sane",
         long_description=long_description,
         long_description_content_type="text/markdown",
+        license="MIT",
+        keywords="make makefiles cmd utility cli",
         url="https://github.com/mikeevmm/sane",
+        py_modules=["sane"],
         packages=setuptools.find_packages(),
         classifiers=[
             "Programming Language :: Python :: 3",
