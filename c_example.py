0e13b5d66ced22285eab1274be40cb80ba59ff74
mikeevmm <miguelmurca@gmail.com>
2021-04-14 14:08:25 +0100

    fix: c example

--- c_example.py
+++ c_example.py
@@ -1,28 +1,47 @@
-import subprocess as sp
-from os import makedirs
+import os
+from subprocess import run
 from glob import glob
 
 from sane import *
 
 CC = "gcc"
 EXE = "main"
+SRC_DIR = "src"
+OBJ_DIR = "obj"
 
-c_sources = glob("src/*.c")
+COMPILE_FLAGS = '-g -O2'
 
-for sourcefile in c_sources:
-    objectfile = sourcefile + '.o'
-    @recipe(
-        name=f"compile_{sourcefile}",
-        hooks=["compilation"],
-        target_files=[objectfile],
-        file_deps=[sourcefile])
+# Ensure source and objects directories exist
+os.makedirs(SRC_DIR, exist_ok=True)
+os.makedirs(OBJ_DIR, exist_ok=True)
+
+sources = glob(f'{SRC_DIR}/*.c')
+
+# Define a compile recipe for each source file in SRC_DIR
+for source_file in sources:
+    basename = os.path.basename(source_file)
+    obj_file = f'{OBJ_DIR}/{basename}.o'
+    objects_older_than_source = (
+        sane_file_condition(sources=[source_file], targets=[obj_file]))
+    
+    @recipe(name=source_file,
+            conditions=[objects_older_than_source],
+            hooks=['compile'],
+            info=f'Compiles the file \'{source_file}\'')
     def compile():
-        makedirs("obj/", exist_ok=True)
-        sp.run(f"{CC} -c {sourcefile} -o {objectfile}", shell=True)
+        run(f'{CC} {COMPILE_FLAGS} -c {source_file} -o {obj_file}', shell=True)
 
-@recipe(hook_deps=["compilation"])
+# Define a linking recipe
+@recipe(hook_deps=['compile'],
+        info='Links the executable.')
 def link():
-    obj_files = ' '.join(glob("src/*.o"))
-    sp.run(f"{CC} -o {EXE} {obj_files}", shell=True)
+    obj_files = glob(f'{OBJ_DIR}/*.o')
+    run(f'{CC} {" ".join(obj_files)} -o {EXE}', shell=True)
 
-sane_run(link)
+# Define a run recipe
+@recipe(recipe_deps=[link],
+        info='Runs the compiled executable.')
+def run_exe():
+    run(f'./{EXE}', shell=True)
+
+sane_run(run_exe)
